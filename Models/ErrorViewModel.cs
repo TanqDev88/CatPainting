b095cb695c384803b7819b315a3b109eabf8488c
tanqueta88 <oliverprim783@gmail.com>
2023-08-19 03:32:55 -0300

    The main models are added to the project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Dependency_injection_project_1.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
---
+++ ViewModel/PilotoIndexViewModel.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Race_Track.Models;
+
+namespace Race_Track.ViewModels;
+
+public class PilotoIndexViewModel{
+public List<Piloto> pilotos {get; set; } = new List<Piloto>();
+
+        public String? NameFilter { get; set; }
+
+        public int PilotoId { get; set; }
+        [Display(Name = "Nombre")]        
+
+        public string? PilotoNombre {get; set;}
+        [Display(Name = "Apellido")]        
+
+        public string? PilotoApellido {get; set;}
+        [Display(Name = "Dni")]        
+
+        public int PilotoDni {get; set;}
+        // public LicenciaType InstructorTipoLicencia { get; set; }
+        [Display(Name = "Numero registro")]        
+
+        [Required(ErrorMessage ="Debe ingresar el Numero de licencia")]
+        public int PilotoNumeroLicencia {get; set;}
+        [DataType("Date", ErrorMessage="Fecha no valida, debe ser dd/MM/yyyy")]
+        [Display(Name = "Fecha de vencimiento")]        
+
+        public DateTime PilotoExpedicion {get;set;}
+        [Display(Name = "Es propietario?")]        
+
+        public bool PilotoPropietario {get;set;} = true;       
+
+        public int VehiculoId { get; set; }
+        [Display(Name = "Vehiculo")]           
+
+        public virtual Vehiculo? Vehiculo { get; set; }    
+
+}
